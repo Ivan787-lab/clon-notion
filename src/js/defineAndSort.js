c476822cc631895c0448a96b53718a61a429679b
Ivan <ivanulanovskij143@gmail.com>
2021-07-02 18:59:58 +0500

    in conditional branches redoned condition for russian words< redone accordeon

--- src/js/defineAndSort.js
+++ src/js/defineAndSort.js
@@ -1,49 +1,57 @@
 
-function accordeon() {
-    const sortBtn = document.querySelector("#sort")
+function accordeon(sortBtnClass,startWord,containerClass,maxHeight,aLotOfInfo) {
+    const sortBtn = document.querySelector(sortBtnClass)
     let switcher = false
 
     sortBtn.addEventListener('click', () => {
         if (!switcher) {
-            sortBtn.innerHTML += ' by';
+            sortBtn.innerHTML += ' по';
             switcher = true;
-            document.querySelector(".table-container__sort-variants").style.maxHeight = '350px';
+            document.querySelector(containerClass).style.maxHeight = maxHeight;
+            if (aLotOfInfo) {
+                document.querySelector(containerClass).style.marginTop = '15px';
+                document.querySelector(containerClass).style.overflow = 'auto';      
+            }
         } else {
-            sortBtn.innerHTML = 'Sort'
+            sortBtn.innerHTML = startWord
             switcher = false;
-            document.querySelector(".table-container__sort-variants").style.maxHeight = null
+            document.querySelector(containerClass).style.maxHeight = null
         }
     });
 } // простая функция для кнопки "Сортировать"
 
 function defineAndSort(array) {
-    let method = document.querySelector('input[name = "variant"]:checked').nextElementSibling.innerHTML.toLowerCase() // здесь присваиваю переменной значение следущего элемента после инпута на который кликнули
 
-    if (method == 'name') {
+    try {
+        var method = document.querySelector('input[name = "variant"]:checked').nextElementSibling.innerHTML.toLowerCase() // здесь присваиваю переменной значение следущего элемента после инпута на который кликнули
+    } catch (error) {}
+    // завернул объявление переменной в trycatch во избежаниии ощибок если пользователь ничего не выбрал но нажал на кнопку
+
+    if (method == 'имени') {
         array.sort((a, b) => {
             if (a.name > b.name) return 1
             if (a.name < b.name) return -1
             return 0
         })
-    } else if (method == 'status') {
+    } else if (method == 'статусу') {
         array.sort((a, b) => {
             if (a.status > b.status) return 1
             if (a.status < b.status) return -1
             return 0
         })
-    } else if (method == 'price') {
+    } else if (method == 'оценочной стоимости') {
         array.sort((a, b) => {
             if (a.price < b.price) return 1
             if (a.price > b.price) return -1
             return 0
         })
-    } else if (method == 'confidence') {
+    } else if (method == 'доверию') {
         array.sort((a, b) => {
             if (a.confidence < b.confidence) return 1
             if (a.confidence > b.confidence) return -1
             return 0
         })
-    } else if (method == 'priority') {
+    } else if (method == 'приориету') {
         array.sort((a, b) => {
             if (a.priority < b.priority) return 1
             if (a.priority > b.priority) return -1
