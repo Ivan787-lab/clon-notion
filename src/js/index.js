720dd9cfa8ec724095d9dedd3a5b94e262c29e4f
Ivan <ivanulanovskij143@gmail.com>
2021-07-02 22:35:40 +0500

    data imported for call function with this argument

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import '../css/style.css'
 import '../css/style.scss'
-import createTable from './creator'
+import { createTable, data } from './creator'
 import { accordeon } from './defineAndSort'
-createTable()
+createTable(data)
 // немного переделана функция аккордеон для возможно ее многократного использования в других блоках
 accordeon('#sort', 'Сортировка', '.table-container__tools', '60px', false)
-accordeon('#filter', 'Фильр', '.table-container__tools', '60px', false)
+accordeon('#filter', 'Фильтр', '.table-container__tools', '60px', false)
