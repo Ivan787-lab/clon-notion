34b238a247c992f53eb2f3300f83bfb47d71ca76
Ivan <ivanulanovskij143@gmail.com>
2021-07-02 20:36:17 +0500

    repeatedly used accordeon

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,6 @@ import '../css/style.scss'
 import createTable from './creator'
 import { accordeon } from './defineAndSort'
 createTable()
-accordeon('#sort', 'Сортировка', '.table-container__sort-variants', '350px', false)
 // немного переделана функция аккордеон для возможно ее многократного использования в других блоках
+accordeon('#sort', 'Сортировка', '.table-container__tools', '60px', false)
+accordeon('#filter', 'Фильр', '.table-container__tools', '60px', false)
