a78fff5556c0748d34a9c4d946dd2a1be792f606
Ivan <ivanulanovskij143@gmail.com>
2021-07-02 18:58:35 +0500

    redone function accordeon for multiple use

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../css/style.css'
 import '../css/style.scss'
 import createTable from './creator'
-import {accordeon} from './defineAndSort'
+import { accordeon } from './defineAndSort'
 createTable()
-accordeon()
+accordeon('#sort', 'Сортировка', '.table-container__sort-variants', '350px', false)
+// немного переделана функция аккордеон для возможно ее многократного использования в других блоках
