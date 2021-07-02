af9fb221ccda13e6f8b3dd6404a95053dd6cb4e6
Ivan <ivanulanovskij143@gmail.com>
2021-07-02 22:37:09 +0500

    function for filter done

---
+++ src/js/defineAndFilter.js
@@ -0,0 +1,39 @@
+
+export default function defineAndFilter(array) {
+    try {
+        var method = document.querySelector('input[name = "variant"]:checked').nextElementSibling.innerHTML.toLowerCase() // здесь присваиваю переменной значение следущего элемента после инпута на который кликнули
+    } catch (error) { }
+    // завернул объявление переменной в trycatch во избежаниии ощибок если пользователь ничего не выбрал но нажал на кнопку
+    if (method == 'имени') {
+        array.sort((a, b) => {
+            if (a.name > b.name) return 1
+            if (a.name < b.name) return -1
+            return 0
+        })
+    } else if (method == 'статусу') {
+        array.sort((a, b) => {
+            if (a.status > b.status) return 1
+            if (a.status < b.status) return -1
+            return 0
+        })
+    } else if (method == 'оценочной стоимости') {
+        array.sort((a, b) => {
+            if (a.price < b.price) return 1
+            if (a.price > b.price) return -1
+            return 0
+        })
+    } else if (method == 'доверию') {
+        array.sort((a, b) => {
+            if (a.confidence < b.confidence) return 1
+            if (a.confidence > b.confidence) return -1
+            return 0
+        })
+    } else if (method == 'приориету') {
+        array.sort((a, b) => {
+            if (a.priority > b.priority) return 1
+            if (a.priority < b.priority) return -1
+            return 0
+        })
+    }
+    // 
+}
