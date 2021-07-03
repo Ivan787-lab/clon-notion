5fd99992ba529b6122ca5479b1fd5aa3d370d7b4
Ivan <ivanulanovskij143@gmail.com>
2021-07-03 10:37:20 +0500

    minor changes have been made

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import '../css/style.css'
 import '../css/style.scss'
+import accordeon from './accordeon'
 import { createTable, data } from './creator'
-import { accordeon } from './defineAndSort'
 createTable(data)
 // немного переделана функция аккордеон для возможно ее многократного использования в других блоках
-accordeon('#sort', 'Сортировка', '.table-container__tools', '60px', false)
-accordeon('#filter', 'Фильтр', '.table-container__tools', '60px', false)
+accordeon('#sort',  '.table-container__tools-variants', '60px', false)
+accordeon('#filter',  '.table-container__tools-variants', '60px', false)
