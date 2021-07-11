32880c55ad37e6484c16a88af09ac739c0125600
Ivan <ivanulanovskij143@gmail.com>
2021-07-11 18:28:16 +0500

    removed extra parameters

--- src/js/accordeon.js
+++ src/js/accordeon.js
@@ -1,7 +1,5 @@
-export default function accordeon(sortBtn, containerClass, maxHeight, aLotOfInfo) {
+export default function accordeon(sortBtn) {
     const btn = document.querySelector(sortBtn)
-    let switcher = false
-
     btn.addEventListener('click', () => {
         let allActiveBtns = document.querySelectorAll('.tools-bar__tool_active')
         btn.classList.add('tools-bar__tool_active')
@@ -9,18 +7,6 @@ export default function accordeon(sortBtn, containerClass, maxHeight, aLotOfInfo
             for (let i = 0; i < allActiveBtns.length; i++) {
                 allActiveBtns[i].classList.remove('tools-bar__tool_active')
             }
-        }
-
-        if (!switcher) {
-            switcher = true;
-            document.querySelector(containerClass).style.maxHeight = maxHeight;
-            if (aLotOfInfo) {
-                document.querySelector(containerClass).style.marginTop = '15px';
-                document.querySelector(containerClass).style.overflow = 'auto';
-            }
-        } else {
-            switcher = false;
-            document.querySelector(containerClass).style.maxHeight = null
         }
     });
 } // простая функция для кнопки "Сортировать"
