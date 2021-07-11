5c409b2fded8d61c79ab2b3ae6d0c531a27f61d1
Ivan <ivanulanovskij143@gmail.com>
2021-07-11 18:25:07 +0500

    redone accordeon function

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,5 @@ import accordeon from './accordeon'
 import { createTable, data } from './creator'
 createTable(data)
 // немного переделана функция аккордеон для возможно ее многократного использования в других блоках
-accordeon('#sort',  '.table-container__tools-variants', '60px', false)
-accordeon('#filter',  '.table-container__tools-variants', '60px', false)
+accordeon('#sort')
+accordeon('#filter')
